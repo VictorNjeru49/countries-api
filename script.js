b891cb14da575233e0a81f64d763b05febd08c00
victornjeru49 <victornjeru49@gmail.com>
2024-05-24 22:13:09 +0300

    comit social

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const apis = 'https://restcountries.com/v3.1/all';
-const countriesContainer = document.getElementById('country'); // Assuming 'country' is the ID of the container element
+
+const countriesContainer = document.getElementById('country'); 
 const searchInput = document.getElementById('search');
 const regions = document.getElementById('regionsFilter');
 const darkMode = document.getElementById('darkMode');
 
 let countriesData = [];
 
-fetch(apis)
+fetch('data.json')
     .then(response => {
         if (!response.ok) {
             throw new Error('ERROR 404');
@@ -15,13 +15,13 @@ fetch(apis)
     })
     .then(data => {
         countriesData = data;
-        displayCountries(countriesData);
+        displyCountries(countriesData);
     })
     .catch(error => {
-        console.error('Error fetching data:', error);
+        console.error('Errors:', error);
     });
 
-function displayCountries(countries) {
+function displyCountries(countries) {
     countriesContainer.innerHTML = '';
     countries.forEach(country => {
         const countryCard = document.createElement('div');
@@ -29,7 +29,7 @@ function displayCountries(countries) {
         countryCard.innerHTML = `
             <img src="${country.flags.png}" alt="${country.name.common} Flag">
             <div class="details">
-                <h2>${country.name.common}</h2>
+                <h2>${country.name}</h2>
                 <p>Population: ${country.population.toLocaleString()}</p>
                 <p>Region: ${country.region}</p>
                 <p>Capital: ${country.capital ? country.capital[0] : 'N/A'}</p>
@@ -41,26 +41,31 @@ function displayCountries(countries) {
         countriesContainer.appendChild(countryCard);
     });
 
-
+    //to use the country search box
     searchInput.addEventListener('input', () => {
         const searchTerm = searchInput.value.toLowerCase('search');
-        const filteredCountries = countriesData.filter(country => country.name.common.toLowerCase().includes(searchTerm));
-        displayCountries(filteredCountries);
+        const filteredCountries = countriesData.filter(country => country.name.toLowerCase().includes(searchTerm));
+        displyCountries(filteredCountries);
     });
+
+    //to filter countries
     const regions=document.getElementById('regionFilter')
     regions.addEventListener('change', () => {
-        const selectedRegion = regions.value;
-        if (selectedRegion === 'all') {
-            displayCountries(countriesData);
+        const selecte = regions.value;
+        if (selecte === 'all') {
+            displyCountries(countriesData);
         } else {
-            const filteredCountries = countriesData.filter(country => country.region === selectedRegion);
-            displayCountries(filteredCountries);
+            const filteredCountries = countriesData.filter(country => country.region === selecte);
+            displyCountries(filteredCountries);
         }
     });
+    
     
 }
 
 
+
+//change the darkmode option to light mode
 document.getElementById('darkMode').addEventListener('click', function() {
     var body = document.body;
     var button = this;
@@ -69,17 +74,20 @@ document.getElementById('darkMode').addEventListener('click', function() {
         body.classList.remove('darkMode');
         body.classList.add('light-mode');
         button.textContent = 'Dark Mode';
-        body.style.backgroundColor ="#FFFFFF";
+        body.style.backgroundColor ="rgb(169, 157, 157)";
     } else {
         body.classList.remove('light-mode');
         body.classList.add('darkMode');
-        button.textContent = 'Light Mode';
+        button.textContent = 'Dark Mode';
         body.style.backgroundColor ="#1a2127";
     }
 });
 
 document.getElementById('back').addEventListener('click', function() {
-    document.getElementById('countryCard').style.display = 'none';
-    document.getElementById('country').style.display = 'block';
+    
+    if (countryCard.checked) {
+        details.style.display = "none";
+    }
+   countryCard.style.display = 'none';
+    country.style.display = 'flex';
 });
-
